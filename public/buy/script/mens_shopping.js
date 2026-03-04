42a03fdb4b1f28e8f6f83f68312fac6c031e87fa
woniw <wonisec@gmail.com>
2026-03-04 07:15:33 -0700

    new mens clothing + completed view script

--- public/buy/script/mens_shopping.js
+++ public/buy/script/mens_shopping.js
@@ -1,13 +1,9 @@
 import { locate_clothing } from "../../../utils/locate_clothing.js";
 
-let nike_view = document.getElementById("nike_view");
+let view_nike = document.getElementById("nike_view");
 
-//view file elements
-let view_item_image = document.querySelector("item_image");
-let view_item_label = document.querySelector("item_label");
-let view_description_text = document.querySelector("description_text");
-
-nike_view.onclick = () => {
-    locate_clothing("nike")
-    window.location.replace("/public/buy/view_item.html")
-}
+view_nike.addEventListener("click", () => {
+    locate_clothing("nike");
+    window.location.replace('/public/buy/view_item.html');
+    }
+)
